848cf4a14d461ccf812c18e1eb29a520ca7c5874
Amogh Firke <amogh.firke@gmail.com>
2024-06-23 11:05:47 +0530

    Home page completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Amogh' };
+
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and all of your important transactions."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
-export default Home
+export default Home;
